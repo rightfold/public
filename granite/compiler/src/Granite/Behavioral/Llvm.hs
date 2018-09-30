2de1a7a99984c64db0772a0ef068a1b045d5085e
Foldr <rightfold+kloe@gmail.com>
2018-09-30 16:49:33 +0200

    [granite] Generate lambda bodies

--- granite/compiler/src/Granite/Behavioral/Llvm.hs
+++ granite/compiler/src/Granite/Behavioral/Llvm.hs
@@ -13,14 +13,16 @@ module Granite.Behavioral.Llvm
   , buildExpression
   ) where
 
-import Control.Lens ((<<%=), at, makeLenses, to, view)
+import Control.Lens ((^.), (<<%=), at, makeLenses, to, view)
 import Control.Monad.Error.Class (MonadError, throwError)
 import Control.Monad.Reader.Class (MonadReader)
 import Control.Monad.State.Class (MonadState)
+import Control.Monad.Trans.RWS (evalRWST)
 import Data.ByteString.Short (ShortByteString)
 import Data.Foldable (for_)
 import Data.HashMap.Strict (HashMap)
 import Data.List (genericLength)
+import Data.Traversable (for)
 import LLVM.AST (Operand)
 import LLVM.IRBuilder (MonadIRBuilder, MonadModuleBuilder)
 
@@ -51,24 +53,16 @@ data Variable
 data Rts =
   Rts
     { _rtsConstructLambda :: Operand
-    , _rtsCallLambda      :: Operand }
+    , _rtsCallLambda      :: Operand
+    , _rtsValuePointers   :: Operand }
 $(makeLenses ''Rts)
 
 data Environment =
   Environment
-    { -- |
-      -- The variables that are in scope, with their corresponding LLVM values.
-      _envVariables :: HashMap Name Variable
-
-      -- |
-      -- The heap to use for memory management.
-    , _envHeap :: Operand
-
-    , _envRts :: Rts
-
-      -- |
-      -- The prefix to use for names of lambda implementations.
-    , _envLambdaNamePrefix :: ShortByteString }
+    { _envRts              :: Rts
+    , _envLambdaNamePrefix :: ShortByteString
+    , _envHeap             :: Operand
+    , _envVariables        :: HashMap Name Variable }
 $(makeLenses ''Environment)
 
 data State =
@@ -81,7 +75,7 @@ freshLambdaName :: (MonadReader Environment m, MonadState State m)
 freshLambdaName = do
   prefix <- view envLambdaNamePrefix
   uniqueId <- stateNextLambdaId <<%= succ
-  pure (prefix <> "$" <> sbshow uniqueId)
+  pure (prefix <> "$lambda" <> sbshow uniqueId)
   where sbshow = BS.S.toShort . BS.C8.pack . show
 
 --------------------------------------------------------------------------------
@@ -110,19 +104,46 @@ buildExpression (Expression position payload) = case payload of
 
   LambdaExpression parameter body -> do
     -- Retrieve used infrastructure.
-    captures <- view (envVariables . to lambdaCaptures)
+    rts       <- view envRts
+    variables <- view envVariables
+    captures  <- view (envVariables . to lambdaCaptures)
 
     -- Define lambda implementation.
     name <- freshLambdaName
-    impl <- IRB.function (IR.Name name) lambdaParams valueType $
-              \[heap, self, argument] ->
-                -- TODO: Ensure entry basic block.
-                -- TODO: Set up new environment.
-                -- TODO: Generate ret instruction.
-                undefined
+    impl <- IRB.function (IR.Name name) lambdaParams valueType
+                         (irbFunctionCallback rts variables captures name)
 
     -- Construct lambda value.
     buildLambdaConstruction impl (snd <$> captures)
+    where
+    irbFunctionCallback :: ( MonadError Error m, MonadIRBuilder m
+                           , MonadModuleBuilder m )
+                        => Rts -> HashMap Name Variable -> [(Name, Operand)]
+                        -> ShortByteString -> [Operand] -> m ()
+    irbFunctionCallback rts variables captures name [heap, self, argument] =
+      let
+        globals :: [(Name, Operand)]
+        globals = [ (n, o) | (n, Global o) <- HashMap.toList variables ]
+
+        args :: BuildLambdaBodyArgs
+        args = BuildLambdaBodyArgs
+          { -- Configuration
+            blRts = rts
+          , blLambdaNamePrefix = name
+
+            -- Lambda arguments
+          , blHeap     = heap
+          , blSelf     = self
+          , blArgument = argument
+
+            -- Code generation
+          , blGlobals   = globals
+          , blCaptures  = fst <$> captures
+          , blParameter = parameter
+          , blBody      = body
+          }
+      in
+        buildLambdaBody args
 
   ForeignExpression source type_ ->
     undefined
@@ -140,6 +161,43 @@ buildGlobalGet global = do
 
 --------------------------------------------------------------------------------
 -- Lambdas
+
+data BuildLambdaBodyArgs =
+  BuildLambdaBodyArgs
+    { -- Configuration
+      blRts :: Rts
+    , blLambdaNamePrefix :: ShortByteString
+
+      -- Lambda arguments
+    , blHeap     :: Operand
+    , blSelf     :: Operand
+    , blArgument :: Operand
+
+      -- Code generation
+    , blGlobals   :: [(Name, Operand)]
+    , blCaptures  :: [Name]
+    , blParameter :: Name
+    , blBody      :: Expression 0 }
+
+-- |
+-- Build the body of a lambda.
+buildLambdaBody :: (MonadError Error m, MonadIRBuilder m, MonadModuleBuilder m)
+                => BuildLambdaBodyArgs -> m ()
+buildLambdaBody bl = do
+  let globals = [ (n, Global o) | (n, o) <- blGlobals bl ]
+  locals <- buildLambdaCapturesToLocals (blRts bl) (blCaptures bl) (blSelf bl)
+  let parameters = [(blParameter bl, Local (blArgument bl))]
+  let variables = HashMap.fromList (globals <> locals <> parameters)
+
+  let env = Environment
+        { _envRts              = blRts bl
+        , _envLambdaNamePrefix = blLambdaNamePrefix bl
+        , _envHeap             = blHeap bl
+        , _envVariables        = variables }
+
+  let build = IRB.ret =<< buildExpression (blBody bl)
+  ((), ()) <- evalRWST build env (State 0)
+  pure ()
 
 -- |
 -- Build a construction of a lambda given its implementation and captured
@@ -186,6 +244,16 @@ lambdaParams = (, IRB.NoParameterName) <$> [heapType, valueType, valueType]
 -- construction site, not those inside the lambda body.
 lambdaCaptures :: HashMap Name Variable -> [(Name, Operand)]
 lambdaCaptures vs = [ (n, o) | (n, Local o) <- HashMap.toList vs ]
+
+-- |
+-- Read the captures of a lambda into locals.
+buildLambdaCapturesToLocals :: (MonadIRBuilder m, MonadModuleBuilder m)
+                            => Rts -> [Name] -> Operand -> m [(Name, Variable)]
+buildLambdaCapturesToLocals rts captures self = do
+  pointers <- IRB.call (rts ^. rtsValuePointers) [(self, [])]
+  for (captures `zip` [0 ..]) $ \(capture, i) -> do
+    pointer <- IRB.gep pointers =<< sequence [IRB.int64 0, IRB.int64 i]
+    pure (capture, Local pointer)
 
 --------------------------------------------------------------------------------
 -- Types
